1e786c43a4e392def379538c847159c82554efb1
evgenevK <evgenev.kostya@icloud.com>
2023-12-16 20:44:59 +0300

    Калькулятор по высчитываниям

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello world ');
+const startMoney = 12000;
+const rate = 0.07;
+const housePrice = 13500;
+const depositLengt = 24;
+
+const resultSum = startMoney * (1 + rate / 12) ** depositLengt;
+
+
+
+if (resultSum > housePrice) {
+    console.log(`Вы накопили: ${resultSum}, остаток - ${resultSum - housePrice}`);
+} else {
+    console.log(`Вы накопили: ${resultSum}, необходимо еще накопить: ${resultSum - housePrice}`);
+}
